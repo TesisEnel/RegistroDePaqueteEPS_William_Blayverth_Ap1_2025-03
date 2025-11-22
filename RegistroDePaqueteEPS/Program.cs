5dac1f9ac400457c9ec803fcc2b3c7c48fb239c9
William Rodriguez <warodriguezv@gmail.com>
2025-11-21 23:37:10 -0400

    Main layout mejorados y division de paginas entre admin y usuario, incluyendo la creacion de inicio y de paquete.

--- RegistroDePaqueteEPS/Program.cs
+++ RegistroDePaqueteEPS/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using RegistroDePaqueteEPS.Components;
 using RegistroDePaqueteEPS.DAL;
+using RegistroDePaqueteEPS.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,8 @@ builder.Services.AddRazorComponents()
 
 var ConStr = builder.Configuration.GetConnectionString("ConStr");
 builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlite(ConStr));
+
+builder.Services.AddScoped<PaquetesService>();
 
 var app = builder.Build();
 
